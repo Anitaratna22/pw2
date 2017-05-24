46f27159b66f502553d521755e8512e9e1e8ea2e
Anitaratna22 <anitaratna@student.untan.ac.id>
2017-05-24 10:05:31 +0700

    Pembaharuan tanggal

--- pages/utama.php
+++ pages/utama.php
@@ -1,13 +1,13 @@
 <!-- First Blog Post -->
                 <?php foreach ($isi_berita as $ib): ?>
                     
-                <h2 align="justify">
+                <h2>
                     <a href="#"><?php echo $ib['judul'] ?></a>
                 </h2>
                 <p class="lead">
                     by <a href="index.php">Start Bootstrap</a>
                 </p>
-                <p><i class="fa fa-clock-o"></i> Posted on August 28, 2013 at 10:00 PM</p>
+                <p><i class="fa fa-clock-o"></i> Posted on <?php echo date('d-F-Y',strtotime($ib['tanggal']));?></p>
                 <hr>
                 
                 <hr>
